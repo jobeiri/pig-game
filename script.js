d8b244c18eea365801991c2aebe27d43c3dce395
Amir Hossein Jobeiri <jobeiri.programmer@gmail.com>
2020-12-17 01:57:43 -0800

    19. Resetting the Game

--- script.js
+++ script.js
@@ -13,15 +13,25 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting condition
-score0Element.textContent = 0;
-score1Element.textContent = 0;
-diceElement.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting condition
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0Element.textContent = 0;
+  score1Element.textContent = 0;
+  current0Element.textContent = 0;
+  current1Element.textContent = 0;
+  diceElement.classList.add('hidden');
+  player0Element.classList.remove('player--winner');
+  player1Element.classList.remove('player--winner');
+  player0Element.classList.add('player--active');
+  player1Element.classList.remove('player--active');
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -30,6 +40,8 @@ const switchPlayer = function () {
   player0Element.classList.toggle('player--active');
   player1Element.classList.toggle('player--active');
 };
+
+btnNew.addEventListener('click', init);
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -76,3 +88,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+init();

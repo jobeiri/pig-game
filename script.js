9bdfd58dd1b9665efcb6f876fbf938562e691fd3
Amir Hossein Jobeiri <jobeiri.programmer@gmail.com>
2020-12-17 00:49:58 -0800

    16. Rolling the Dice

--- script.js
+++ script.js
@@ -3,9 +3,34 @@
 // Selecting elements
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
-const diceElement = document.querySelector('.dice');
+const current0Element = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
 
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting condition
 score0Element.textContent = 0;
 score1Element.textContent = 0;
-
 diceElement.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display dice
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0Element.textContent = currentScore;
+  } else {
+    // switch to next player
+  }
+});
